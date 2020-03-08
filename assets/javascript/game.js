0837d931ed396cc2c262b9577d466f9dad4320c1
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-03-07 22:41:51 -0500

    worked further into guessing game, debugging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,132 +6,121 @@
 //with each guess, letter gets stored in guessedArray, whether it wa correct or incorrect guess
 //correct guess does not decrease chances variable
 //if chances reaches 0, game resets
-
 //if player guesses word correctly within chances limit, wins variable increase by + 1, game resets. 
 
 //bonus, play winning tune to match band/song when win is guessed
 //bonus, play losing horn (price is right horn?) when chances reach 0
 
 //word array for cpu to choose winning word to from
-var wordArray = ["nirvana","pearl jam","soundgarden","alice in chains","in utero", "ten", "evenflow","meat puppets","smashing pumpkins","tool","flannel","doc martens","airwalks","mtv unplugged"];
-var lettersGuessedArray = [];
+var wordArray = ["nirvana", "pearl jam", "soundgarden", "alice in chains", "in utero", "ten", "evenflow", "meat puppets", "smashing pumpkins", "tool", "flannel", "doc martens", "airwalks", "mtv unplugged"];
+var guessedLettersArray = [];
+var mysteryWord = [];
+var usedWinningWords = [];
+var winningWord;
 var wins = 0;
-var Chances = 10;
-var winningWord = " ";
+var guessesLeft = 10;
+// var winSong = 
+// var loseSong =
+// var championSong = 
+
+// id's that are in html
+//  id="get-started">Press Any Key To Get Started!</div>
+// id="wins">Wins:</div>
+//  id="winningWord">Mystery Word:</div>
+//  id="guessesleft">Chances:</div>
+//  id="guessessofar">Letters Guessed:</div>
+
+function startGame() { // start new game with default settings
+
+winningWord = wordArray[Math.floor(Math.random() * wordArray.length)].toUpperCase();
+
+//set winningWord to underscores
+for (var i = 0; i < winningWord.length; i++) {
+    if (winningWord[i] === " ") {
+        mysteryWord.push(" ")
+    }
+    else {
+        mysteryWord.push("_");
+    }
+}
+updateDisplay();
+};
 
 
-//event listener to start new game, reads key press
-window.addEventListener("keypress",onkeypress,false);
-//call start game function
-startGame();
-//functions possibly needed:
-//to initialize game and to reset game
-function startGame() {
-//cpu selects random word from wordArray
-    selectWord();
-//takes winning word    
-    wordUnderscores();
-    updateWins();
-    chances = startChances;
-    updateChances();
-    updateMaskedWord();
-    lettersGuessedArray=[];
-    displayGuessed();
-}
-
-//functions to update displays at start of game
-//updates wins in html
-function updateWins () {
-    document.getElementById("wins").innerHTML = String(wins);
-}
-//updates chances in html
-function updateChances () {
-    document.getElementById("chances").innerHtml = String(chances);
-}
-//subtracts chances by 1 when user guesses incorrectly
-function subtractChances () {
-    chances--;
-    updateChances();
-}
-//updates Letters guessed in html
-function displayGuessed () {
-    if (lettersGuessedArray.length === 0) {
-        document.getElementById("guessed").innerHTML = "_";
-    } else {
-        document.getElementById("guessed").innerHTML = "_";
-        for (i = 0; i < lettersGuessedArray.length; i++) {
-            document.getElementById("guessed").innerHTML += String(lettersGuessedArray[i]);
+function resetGame () {
+    //if player guesses all words
+    if (usedWinningWords.length === wordArray) {
+        championSong.play();
+        usedWinningWords = [];
+        wins = 0;
+    }
+    else {
+        //retrieve new word
+        winningWord = wordArray[Math.floor(Math.random() * wordArray.length)].toUpperCase();
+        console.log(winningWord);
+        if (usedWinningWords.includes(winningWord) === true) {
+            resetGame();
+    }
+    guessedLettersArray = [];
+    mysteryWord = [];
+    //reset guesses word
+    for (var i=0; i < winningWord.length; i++) {
+        if (wordToMatch[i] === " ") {
+            guessingWord.push(" ")
+          } 
+          else {
+            guessingWord.push("_");
+          }
         }
-    }
-}
-//updates mystery word in html
-function updateMaskedWord() {
-        document.getElementById("wordToGuess").innerHTML = "",
-        for (i = 0; i < maskedWord.length; i++) {
-            if (isNaN(maskedWord[i]) == true) {
-                document.getElementById("wordToGuess").innerHTML += " _";
-            } else {
-                document.getElementById("wordToGuess").innerHTML += " " + mysteryWord[i];
-            }
-        }
-}
-
-function checkForWin() {
-    if (maskedWord == winningMask) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  function selectWord() {
-    mysteryWord = wordArray[(Math.floor((Math.random() * wordArray.length) + 1)) - 1];
-    maskedWord = mysteryWord;
-    console.log(mysteryWord); // used for debugging
-  }
-  
-  function setWinningMask() {
-    winningMask = ""; // reset to blank
-    for (i = 0; i < mysteryWord.length; i++) { // set based on mysteryWord length
-      winningMask += "1";
-    }
-  }
-  function isLetterInWord(letter) {
-    if (mysteryWord.indexOf(letter) == -1) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-  function onKeyPress(key) {
-    letter = key.key
-    displayLastKeyPressed(letter);
-    if (isLetterInWord(letter)) { // check if letter is in word
-      replaceAllOccurences(letter); // mask letters in word
-    } else { // if letter is NOT in word
-      if (hasLetterBeenUsed(letter) == false) { // letter NOT in word; has not been used
-        lettersGuessedArray.push(letter)
-        subtractChances();
-      } else {
-        // letter NOT in word; has already been used
+        updateDisplay();
       }
+};
+
+function updateDisplay () {
+    document.getElementById("wins").innerText = wins;
+    document.getElementById("winningWord").innerText = mysteryWord.join("");
+    document.getElementById("remainingChances").innerText = guessesLeft;
+    document.getElementById("guessesSoFar").innerText = guessedLettersArray.join(" ");
+};
+
+document.onkeydown = function(event) {
+    checkForLetter(event.key.toUpperCase());
+};
+
+var isLetter = function(ch) {
+    return typeof ch === "string" && ch.length === 1 && (ch >= "a" && ch <= "z" || ch >= 'A' && ch <= "Z");
+};
+
+function checkForLetter(letter) {
+    var foundLetter = false;
+
+    for (var i=0; i < winningWord.length; i++) {
+        if (letter === winningWord[i]) {
+            mysteryWord[i] = letter;
+            foundLetter = true;
+        if (mysteryWord.join("") === winningWord) {
+            wins++
+            usedWinningWords.push(winningWord);
+            console.log(usedWinningWords);
+            winSound.play();
+            updateDisplay();
+        }
     }
-    displayGuessed();
-    displayMaskedWord();
-    if (checkForWin() == true) { // if user wins, increment wins count, start new game
-      wins++;
-      updateWins();
-      startGame();
+}
+if (foundLetter === false) {
+    if (guessedLettersArray.includes(letter) === false) {
+        guessedLettersArray.push(lettr)
+        guessesLeft--
     }
-    if (guessesLeft == 0) { // if zero guesses left, start new game
-      startGame();
+    if (guessesLeft === 0) {
+        usedWinningWords.push(winningWord);
+        console.log(usedWinningWords);
+        mysteryWord = winningWord.split();
+        loseSound.play();
     }
-  }
-  
-  function hasLetterBeenUsed(letter) {
-    if (lettersGuessedArray.indexOf(letter) == -1) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+}
+updateDisplay();
+};
+
+startGame();
+
