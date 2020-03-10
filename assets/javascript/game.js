473563681edcf31ba33519e94a8567938bf4dbfe
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-03-10 18:06:09 -0400

    added images for game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,115 +12,12 @@
 //bonus, play losing horn (price is right horn?) when chances reach 0
 
 //word array for cpu to choose winning word to from
-var wordArray = ["nirvana", "pearl jam", "soundgarden", "alice in chains", "in utero", "ten", "evenflow", "meat puppets", "smashing pumpkins", "tool", "flannel", "doc martens", "airwalks", "mtv unplugged"];
-var guessedLettersArray = [];
-var mysteryWord = [];
-var usedWinningWords = [];
+var wordArray = ["nirvana", "pearl jam", "smells like teen spirit", "soundgarden", "badmotorfinger",
+"jeremy", "alice in chains", "dirt", "in utero", "meat puppets", "ten", "smashing pumpkins", 
+"flannel", "doc martens", "airwalks", "mtv unplugged"];
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var wrongGuess = [];
+var blank = "";
 var winningWord;
 var wins = 0;
 var guessesLeft = 10;
-// var winSong = 
-// var loseSong =
-// var championSong = 
-
-// id's that are in html
-//  id="get-started">Press Any Key To Get Started!</div>
-// id="wins">Wins:</div>
-//  id="winningWord">Mystery Word:</div>
-//  id="guessesleft">Chances:</div>
-//  id="guessessofar">Letters Guessed:</div>
-
-function startGame() { // start new game with default settings
-
-winningWord = wordArray[Math.floor(Math.random() * wordArray.length)].toUpperCase();
-
-//set winningWord to underscores
-for (var i = 0; i < winningWord.length; i++) {
-    if (winningWord[i] === " ") {
-        mysteryWord.push(" ")
-    }
-    else {
-        mysteryWord.push("_");
-    }
-}
-updateDisplay();
-};
-
-
-function resetGame () {
-    //if player guesses all words
-    if (usedWinningWords.length === wordArray) {
-        // championSong.play();
-        usedWinningWords = [];
-        wins = 0;
-    }
-    else {
-        //retrieve new word
-        winningWord = wordArray[Math.floor(Math.random() * wordArray.length)].toUpperCase();
-        console.log(winningWord);
-        if (usedWinningWords.includes(winningWord) === true) {
-            resetGame();
-    }
-    guessedLettersArray = [];
-    mysteryWord = [];
-    //reset guesses word
-    for (var i=0; i < winningWord.length; i++) {
-        if (wordToMatch[i] === " ") {
-            guessingWord.push(" ")
-          } 
-          else {
-            guessingWord.push("_");
-          }
-        }
-        updateDisplay();
-      }
-};
-
-function updateDisplay () {
-    document.getElementById("wins").innerHTML = wins;
-    document.getElementById("winningWord").innerHTML = mysteryWord.join("");
-    document.getElementById("remainingChances").innerHTML = guessesLeft;
-    document.getElementById("guessesSoFar").innerHTML = guessedLettersArray.join(" ");
-};
-
-document.onkeydown = function(event) {
-    checkForLetter(event.key.toUpperCase());
-};
-
-var isLetter = function(ch) {
-    return typeof ch === "string" && ch.length === 1 && (ch >= "a" && ch <= "z" || ch >= 'A' && ch <= "Z");
-};
-
-function checkForLetter(letter) {
-    var foundLetter = false;
-
-    for (var i=0; i < winningWord.length; i++) {
-        if (letter === winningWord[i]) {
-            mysteryWord[i] = letter;
-            foundLetter = true;
-        if (mysteryWord.join("") === winningWord) {
-            wins++
-            usedWinningWords.push(winningWord);
-            console.log(usedWinningWords);
-            // winSound.play();
-            updateDisplay();
-        }
-    }
-}
-if (foundLetter === false) {
-    if (guessedLettersArray.includes(letter) === false) {
-        guessedLettersArray.push(letter)
-        guessesLeft--
-    }
-    if (guessesLeft === 0) {
-        usedWinningWords.push(winningWord);
-        console.log(usedWinningWords);
-        mysteryWord = winningWord.split();
-        // loseSound.play();
-    }
-}
-updateDisplay();
-};
-
-startGame();
-
